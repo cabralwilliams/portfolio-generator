6093761df674a76ed44173c7e5583e5c6d812080
Cabral Williams <cabral.williams@gmail.com>
2021-10-07 21:26:38 -0500

    abitlity to generate html file added

--- app.js
+++ app.js
@@ -1,25 +1,12 @@
-/*
-var message = 'Hello Node!';
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-var sum = 5 + 3;
+const profileDataArgs = process.argv.slice(2);
 
-console.log(message);
-console.log(sum);
-*/
-//var commandLineArgs = process.argv;
-//console.log(commandLineArgs);
+const [name, github] = profileDataArgs;
 
-var profileDataArgs = process.argv.slice(2,process.argv.length);
-//console.log(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-
-    profileDataArr.forEach(profileItem => {
-        console.log(profileItem);
-    });
-};
-
-printProfileData(profileDataArgs);
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
