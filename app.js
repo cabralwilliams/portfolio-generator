f756da2d04fbe3ff30f6b4e30e95245d9c34f34f
Cabral Williams <cabral.williams@gmail.com>
2021-10-08 10:05:30 -0500

    Validation added to question prompts

--- app.js
+++ app.js
@@ -26,12 +26,28 @@ const promptProject = portfolioData => {
       {
         type: 'input',
         name: 'name',
-        message: 'What is the name of your project?'
+        message: 'What is the name of your project?',
+        validate: nameInput => {
+            if(nameInput) {
+                return true;
+            } else {
+                console.log('Please enter the name of the project.');
+                return false;
+            }
+        }
       },
       {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project (Required)'
+        message: 'Provide a description of the project (Required)',
+        validate: description => {
+            if(description) {
+                return true;
+            } else {
+                console.log('Please enter a description of the project.');
+                return false;
+            }
+        }
       },
       {
         type: 'checkbox',
@@ -42,7 +58,15 @@ const promptProject = portfolioData => {
       {
         type: 'input',
         name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)'
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: pageLink => {
+            if(pageLink) {
+                return true;
+            } else {
+                console.log('Please enter the GitHub link to the project.');
+                return false;
+            }
+        }
       },
       {
         type: 'confirm',
@@ -73,17 +97,46 @@ const promptUser = () => {
             {
                 type: "input",
                 name: "name",
-                message: "What is your name?"
+                message: "What is your name?",
+                validate: nameInput => {
+                    if(nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your name.');
+                        return false;
+                    }
+                }
             },
             {
                 type: "input",
                 name: "github",
-                message: "What is your username for your GitHub account?"
+                message: "What is your username for your GitHub account?",
+                validate: githubInput => {
+                    if(githubInput) {
+                      return true;
+                    } else {
+                        console.log('Please enter the username for your GitHub account.');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'confirm',
+                name: 'confirmAbout',
+                message: 'Would you like to enter some information about yourself for an "About" section?',
+                default: true
             },
             {
                 type: "input",
                 name: "about",
-                message: "Please provide some information about yourself:"
+                message: "Please provide some information about yourself:",
+                when: ({ confirmAbout }) => {
+                    if(confirmAbout) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
             }
         ]
     );
